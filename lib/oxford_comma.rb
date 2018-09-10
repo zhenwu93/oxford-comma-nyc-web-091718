51b7c198c8b27658c68f69b70bf7a2833357a427
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 23:01:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-  if array.length == 3
-    array.join(", ")
+  if array.length >= 3
+    new_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_item)
+    return array.join(", ")
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 1
