b8c1ca068fb86c74626377ecfd6d91bb69f44eec
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 23:01:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length >= 3
+    new_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_item)
+    return array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 1
+    return array[0]
+  end
 end
